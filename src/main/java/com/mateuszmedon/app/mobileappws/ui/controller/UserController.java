0617fc4332cb38b1b974639868d9c0b7bfd5903f
Mateusz Medon <mateuszmedon1@gmail.com>
2020-03-15 19:43:20 +0000

    create request controller, + test with postman

--- src/main/java/com/mateuszmedon/app/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/mateuszmedon/app/mobileappws/ui/controller/UserController.java
@@ -1,8 +1,33 @@
 package com.mateuszmedon.app.mobileappws.ui.controller;
 
+import org.springframework.web.bind.annotation.*;
 
-
+@RestController
+@RequestMapping("users")
 public class UserController {
+
+    @GetMapping
+    public String getUser(){
+        return "You have a user";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "User was created";
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "Update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "Delete user was created";
+    }
+
+
+
 
 
 
