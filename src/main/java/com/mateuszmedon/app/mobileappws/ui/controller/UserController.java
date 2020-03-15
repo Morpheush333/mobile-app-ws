205d4efc87df4d3df323fccef4ff6b96792752e6
Mateusz Medon <mateuszmedon1@gmail.com>
2020-03-15 23:20:24 +0000

    set database configuration, modeling user POST controller

--- src/main/java/com/mateuszmedon/app/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/mateuszmedon/app/mobileappws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.mateuszmedon.app.mobileappws.ui.controller;
 
+import com.mateuszmedon.app.mobileappws.ui.model.request.UserDetailsRequestModel;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +13,7 @@ public class UserController {
     }
 
     @PostMapping
-    public String createUser(){
+    public String createUser(@RequestBody UserDetailsRequestModel userDetails){
         return "User was created";
     }
 
