a81c6a3d6a5acbb952ae4c79dac8cb91be7380ce
Mateusz Medon <mateuszmedon1@gmail.com>
2020-03-19 16:29:33 +0000

    add generate random userId

--- src/main/java/com/mateuszmedon/app/mobileappws/service/impl/UserServiceImpl.java
+++ src/main/java/com/mateuszmedon/app/mobileappws/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.mateuszmedon.app.mobileappws.service.impl;
 import com.mateuszmedon.app.mobileappws.UserRepository;
 import com.mateuszmedon.app.mobileappws.io.entity.UserEntity;
 import com.mateuszmedon.app.mobileappws.service.UserService;
+import com.mateuszmedon.app.mobileappws.shared.Utils;
 import com.mateuszmedon.app.mobileappws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
 
@@ -22,8 +26,10 @@ public class UserServiceImpl implements UserService {
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
+        String publicUserId = utils.generateUserId(30);
+
+        userEntity.setUserId(publicUserId);
         userEntity.setEncryptedPassword("test password");
-        userEntity.setUserId("testUserID");
 
         UserEntity storedUserEntity = userRepository.save(userEntity);
 
