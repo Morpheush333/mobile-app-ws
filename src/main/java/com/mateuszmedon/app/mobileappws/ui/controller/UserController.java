148ec35e6187f0481d283abfad6af431fa76d8a8
Mateusz Medon <mateuszmedon1@gmail.com>
2020-03-20 17:22:33 +0000

    addon json and xml support (get, post)

--- src/main/java/com/mateuszmedon/app/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/mateuszmedon/app/mobileappws/ui/controller/UserController.java
@@ -6,6 +6,7 @@ import com.mateuszmedon.app.mobileappws.ui.model.request.UserDetailsRequestModel
 import com.mateuszmedon.app.mobileappws.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +16,8 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/{id}",
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
     public UserRest getUser(@PathVariable String id){
         UserRest returnValue = new UserRest();
 
@@ -25,7 +27,10 @@ public class UserController {
         return returnValue;
     }
 
-    @PostMapping
+    @PostMapping(
+        consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+        produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }
+    )
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails){
 
         UserRest returnValue = new UserRest();
