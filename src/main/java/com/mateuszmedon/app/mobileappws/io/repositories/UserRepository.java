bef08c88a6f240e5858f80cd89b563fd789d0444
Mateusz Medon <mateuszmedon1@gmail.com>
2020-03-20 14:25:04 +0000

    get user details by getMapping

--- src/main/java/com/mateuszmedon/app/mobileappws/io/repositories/UserRepository.java
+++ src/main/java/com/mateuszmedon/app/mobileappws/io/repositories/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
     UserEntity findByEmail(String email);
+
+    UserEntity findByUserId(String userId);
 }
--- src/main/java/com/mateuszmedon/app/mobileappws/service/impl/UserServiceImpl.java
+++ src/main/java/com/mateuszmedon/app/mobileappws/service/impl/UserServiceImpl.java
@@ -62,6 +62,17 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public UserDto getUserByUserId(String userId) {
+        UserDto returnValue = new UserDto();
+        UserEntity userEntity = userRepository.findByUserId(userId);
+
+        if (userEntity == null) throw new UsernameNotFoundException(userId);
+
+        BeanUtils.copyProperties(userEntity, returnValue);
+        return returnValue;
+    }
+
+    @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UserEntity userEntity = userRepository.findByEmail(email);
 
