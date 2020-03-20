4df8dfd2a4d4b6b4566f3671ac58bdaa87fd5165
Mateusz Medon <mateuszmedon1@gmail.com>
2020-03-20 12:09:26 +0000

    customize authentication url

--- src/main/java/com/mateuszmedon/app/mobileappws/security/WebSecurity.java
+++ src/main/java/com/mateuszmedon/app/mobileappws/security/WebSecurity.java
@@ -24,11 +24,17 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
                 .permitAll()
-                .anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+                .anyRequest().authenticated().and().addFilter(getAuthenticationFilter());
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    protected AuthenticationFilter getAuthenticationFilter() throws Exception {
+        final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+        filter.setFilterProcessesUrl("/users/login");
+        return filter;
     }
 }
