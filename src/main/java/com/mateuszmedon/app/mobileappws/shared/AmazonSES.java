d2cc67591acb75a48983f8fe851ee74f925a2d5a
Mateusz Medon <mateuszmedon1@gmail.com>
2020-04-05 11:20:19 +0100

    fix AmazonSES parameters

--- src/main/java/com/mateuszmedon/app/mobileappws/shared/AmazonSES.java
+++ src/main/java/com/mateuszmedon/app/mobileappws/shared/AmazonSES.java
@@ -9,7 +9,7 @@ import com.mateuszmedon.app.mobileappws.shared.dto.UserDto;
 public class AmazonSES {
 
     // This address must be verified with Amazon SES.
-    final String FROM = "sergey.kargopolov@swiftdeveloperblog.com";
+    final String FROM = "test@gmail.com";
 
     // The subject line for the email.
     final String SUBJECT = "One last step to complete your registration with PhotoApp";
@@ -20,7 +20,7 @@ public class AmazonSES {
     final String HTMLBODY = "<h1>Please verify your email address</h1>"
             + "<p>Thank you for registering with our mobile app. To complete registration process and be able to log in,"
             + " click on the following link: "
-            + "<a href='http://ec2-35-173-238-100.compute-1.amazonaws.com:8080/verification-service/email-verification.html?token=$tokenValue'>"
+            + "http://localhost:8080/verification-service/email-verification.html?token=$tokenValue'>"
             + "Final step to complete your registration" + "</a><br/><br/>"
             + "Thank you! And we are waiting for you inside!";
 
@@ -28,7 +28,7 @@ public class AmazonSES {
     final String TEXTBODY = "Please verify your email address. "
             + "Thank you for registering with our mobile app. To complete registration process and be able to log in,"
             + " open then the following URL in your browser window: "
-            + " http://ec2-35-173-238-100.compute-1.amazonaws.com:8080/verification-service/email-verification.html?token=$tokenValue"
+            + " http://localhost:8080/verification-service/email-verification.html?token=$tokenValue"
             + " Thank you! And we are waiting for you inside!";
 
 
@@ -52,8 +52,8 @@ public class AmazonSES {
     public void verifyEmail(UserDto userDto) {
 
         // You can also set your keys this way. And it will work!
-        //System.setProperty("aws.accessKeyId", "<YOUR KEY ID HERE>");
-        //System.setProperty("aws.secretKey", "<SECRET KEY HERE>");
+//        System.setProperty("aws.accessKeyId", "");
+//        System.setProperty("aws.secretKey", "");
 
         AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard().withRegion(Regions.EU_WEST_1)
                 .build();
